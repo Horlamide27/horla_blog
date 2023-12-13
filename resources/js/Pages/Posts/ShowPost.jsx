a95aefbb907c7f095ed6e37721bec314dd4b8500
chubi-x <chubixax@protonmail.com>
2023-12-13 19:39:26 +0000

    show posts ui for guest users

--- resources/js/Pages/Posts/ShowPost.jsx
+++ resources/js/Pages/Posts/ShowPost.jsx
@@ -3,13 +3,15 @@ import {Link, useForm} from "@inertiajs/react";
 import TextArea from "@/Components/TextArea.jsx";
 import PrimaryButton from "@/Components/PrimaryButton.jsx";
 import InputError from "@/Components/InputError.jsx";
+import Guest from "@/Layouts/GuestLayout.jsx";
 
 export default function ShowPost({auth, post, author}){
-    const isAuthor = auth.user.id === post.user_id;
-    const isAdmin = auth.user.role === 'MANAGER' || auth.user.role === 'ADMIN';
-    const {setData, post:postComment, delete:deleteFn, errors} = useForm({
+    const isAuthor = auth?.user?.id === post?.user_id;
+    const isAdmin = auth?.user?.role === 'MANAGER' || auth?.user?.role === 'ADMIN';
+    const {setData, post:postComment, delete:deleteFn, errors } = useForm({
         content: '',
     });
+    const { delete:deleteFnPost} = useForm({});
     const submit = (e) => {
         e.preventDefault();
         postComment(route('posts.comment.store', {id: post.id}));
@@ -20,42 +22,41 @@ export default function ShowPost({auth, post, author}){
     }
     const deletePost = (e) => {
         e.preventDefault();
-        deleteFn(route('posts.destroy', {id:post.id}));
+        deleteFnPost(route('posts.destroy', {id:post.id}));
     }
-    return (
-        <AuthenticatedLayout user={auth.user}>
-            <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-                <div className="mx-auto max-w-lg">
-                    <div className='flex items-center justify-between my-10 mb-20'>
-                        <h1 className="text-center text-lg font-bold text-red-600 sm:text-2xl">
-                            {post.title}
-                        </h1>
-                        <p className='text-gray-600 text-sm'>
-                            Posted in
-                            <span className='font-bold underline'>
+    const Content = () => {
+        return <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+            <div className="mx-auto max-w-lg">
+                <div className='flex items-center justify-between my-10 mb-20'>
+                    <h1 className="text-center text-lg font-bold text-red-600 sm:text-2xl">
+                        {post.title}
+                    </h1>
+                    <p className='text-gray-600 text-sm'>
+                        Posted in
+                        <span className='font-bold underline'>
                                 {' '}
-                                <Link href={route('animes.show', {id: post.anime.id})}>
+                            <Link href={route('animes.show', {id: post.anime.id})}>
                                     {post.anime.title}
                                 </Link>
-                                {'  '}
+                            {'  '}
                             </span>
 
-                            by {author}
-                        </p>
-                        { (isAuthor || isAdmin) &&
-                            <PrimaryButton onClick={(e) => deletePost(e)}>
+                        by {author}
+                    </p>
+                    { (isAuthor || isAdmin) &&
+                        <PrimaryButton onClick={(e) => deletePost(e)}>
                             <p className=' text-sm'>
                                 Delete
                             </p>
                         </PrimaryButton>
-                        }
+                    }
 
-                    </div>
-                    <div>
-                        <p className="">
-                            {post.body}
-                        </p>
-                    </div>
+                </div>
+                <div>
+                    <p className="">
+                        {post.body}
+                    </p>
+                </div>
                 <div className='flex flex-col justify-between my-10'>
                     <div className='flex items-center space-x-10'>
                         <h1 className="text-center text-md font-bold text-blue-600 sm:text-lg">
@@ -75,21 +76,22 @@ export default function ShowPost({auth, post, author}){
                                             {comment.user.name}
                                         </Link>
                                     </p>
-                                    <PrimaryButton onClick={(e)=>deleteComment(e,comment.id)} >
+                                    { (isAuthor || isAdmin || auth?.user?.id === comment?.user?.id )&&
+                                        <PrimaryButton onClick={(e) => deleteComment(e, comment.id)}>
                                         <p className=' text-sm'>
                                             Delete
                                         </p>
-                                    </PrimaryButton>
+                                    </PrimaryButton>}
                                 </div>
-                             <p className=' text-md'>
-                                {comment.content}
+                                <p className=' text-md'>
+                                    {comment.content}
                                 </p>
                             </div>
                         </div>
                     ))}
                     <form className='mt-10 w-full' onSubmit={(e)=>submit(e)}>
                         <TextArea className='w-full'
-                          onChange={(e) => setData('content', e.target.value)}
+                                  onChange={(e) => setData('content', e.target.value)}
                         />
                         <InputError message={errors.content} className="mt-2"/>
                         <PrimaryButton className='mt-4' >
@@ -98,9 +100,17 @@ export default function ShowPost({auth, post, author}){
 
                     </form>
                 </div>
-                </div>
             </div>
+        </div>
+    }
+    return (
+         auth?.user ?
+    <AuthenticatedLayout user={auth?.user}>
+        {Content()}
+    </AuthenticatedLayout>
+            : <Guest>
+                 {Content()}
+            </Guest>
 
-        </AuthenticatedLayout>
     )
 }
