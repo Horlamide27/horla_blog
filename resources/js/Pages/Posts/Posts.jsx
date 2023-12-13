8eb2e1dd9fad8118c6fcd53ef4a087b4a6c983ec
chubi-x <chubixax@protonmail.com>
2023-12-13 19:40:28 +0000

    show user posts and comments

---
+++ resources/js/Pages/Posts/Posts.jsx
@@ -0,0 +1,17 @@
+import PaginatedContent from "@/Components/PaginatedContent.jsx";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.jsx";
+
+export default function Posts({auth, posts, comments}){
+    console.log(comments
+    )
+    return (
+        <AuthenticatedLayout user={auth.user}>
+            <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+                <h1 className='text-3xl font-bold'>Posts and Comments by {posts[0].user?.name}</h1>
+
+                <PaginatedContent content={posts}/>
+                <PaginatedContent content={comments}/>
+            </div>
+        </AuthenticatedLayout>
+    )
+}
