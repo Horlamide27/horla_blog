bd2ec8a4853f9ed6e7f896fecd8c75cfe741eaa7
chubi-x <chubixax@protonmail.com>
2023-12-13 19:40:57 +0000

    update get user posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use App\Models\Image;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -14,23 +15,25 @@ class PostController extends Controller {
     /**
      * Display a listing of the resource.
      */
-    public function index() {
-        $posts = Post::all();
-        return Inertia::render('Posts/Posts', [
-            'posts' => $posts,
-        ]);
-    }
+//    public function index() {
+//        $posts = Post::all();
+//        return Inertia::render('Posts/Posts', [
+//            'posts' => $posts,
+//        ]);
+//    }
 
     /**
      * show posts for a user
      */
     public function userIndex(string $id) {
         $posts = Post::where("user_id", $id)->get();
-//        foreach ($posts as $post) {
-//            $post->;
-//        }
+        $comments = Comment::where("user_id", $id)->get();
+        foreach ($posts as $post) {
+            $post->user;
+        }
         return Inertia::render('Posts/Posts', [
             'posts' => $posts,
+            'comments' => $comments,
         ]);
     }
 
@@ -70,9 +73,61 @@ class PostController extends Controller {
     public function show(string $id) {
         $post = Post::findorFail($id);
         $post->comments;
+        foreach ($post->comments as $comment) {
+            $comment->user;
+        }
         $post->anime;
 
-class PostController extends Controller
-{
-    //
+        return Inertia::render('Posts/ShowPost', [
+            'post' => $post,
+            'author'=>$post->user->name,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id) {
+        return Inertia::render('Posts/EditPost', [
+            'post' => Post::findorFail($id),
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id) {
+        //validate request
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+        //use edit-post policy to authorize user
+        $post = Post::findorFail($id);
+        $authorized = Gate::inspect('update', $post);
+        if (!$authorized->allowed()) {
+            abort(403, 'You cannot edit this post.');
+        }
+        $post->title = $request->all()["title"];
+        $post->text = $request->all()["body"];
+        $post->save();
+        return Redirect::route('posts.show', ['id' => $post->id]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id) {
+        $post = Post::findorFail($id);
+//        $authorized = Gate::inspect('delete', $post);
+//        if ($authorized->allowed()) {
+            foreach ($post->comments as $comment) {
+                $comment->delete();
+            }
+            $post->delete();
+            return Redirect::route('dashboard');
+//        }
+//        abort(403, 'You cannot delete this post.');
+
+    }
 }
