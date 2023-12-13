f4127f75764ffdaeeeabefc20b1ee6b6087ce380
chubi-x <chubixax@protonmail.com>
2023-12-13 14:38:01 +0000

    create many-to-many relationship btw animes and genres

--- app/Models/Anime.php
+++ app/Models/Anime.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
@@ -20,5 +21,8 @@ class Anime extends Model
     }
     public function cover(): MorphOne {
         return $this->morphOne(Image::class, 'imageable');
+    }
+    public function genres():BelongsToMany{
+        return $this->belongsToMany(Genre::class);
     }
 }
