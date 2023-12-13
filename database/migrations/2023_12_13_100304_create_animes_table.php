06dc573134c617615edda465e0b357575d88ed35
chubi-x <chubixax@protonmail.com>
2023-12-13 14:22:15 +0000

    remove default anime cover

--- database/migrations/2023_12_13_100304_create_animes_table.php
+++ database/migrations/2023_12_13_100304_create_animes_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string("title");
             $table->foreignId("user_id")->constrained();
             $table->text("about");
-            $table->string("cover")->default("https://via.placeholder.com/200");
         });
     }
 
