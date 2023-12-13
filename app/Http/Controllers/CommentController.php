54f1bd462cda259e1bdc1fda27d73810c4b1eabc
chubi-x <chubixax@protonmail.com>
2023-12-13 18:14:07 +0000

    create and delete comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request, string $id) {
+        // validate request
+        $request->validate([
+            'content' => 'required',
+        ]);
+        //create post
+        $comment = new Comment;
+        $comment->content = $request->all()["content"];
+        $comment->user()->associate($request->user()->id);
+        $comment->post()->associate($id);
+        $comment->save();
+        return Redirect::route('posts.show', ['id' => $id]);
+    }
+    public function destroy(string $post_id, string $id) {
+        $comment = Comment::find($id);
+        $comment->delete();
+        return Redirect::route('posts.show', ['id' => $post_id]);
+    }
 }
--- resources/js/Pages/Posts/ShowPost.jsx
+++ resources/js/Pages/Posts/ShowPost.jsx
@@ -1,7 +1,27 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.jsx";
-import {Link} from "@inertiajs/react";
+import {Link, useForm} from "@inertiajs/react";
+import TextArea from "@/Components/TextArea.jsx";
+import PrimaryButton from "@/Components/PrimaryButton.jsx";
+import InputError from "@/Components/InputError.jsx";
 
 export default function ShowPost({auth, post, author}){
+    const isAuthor = auth.user.id === post.user_id;
+    const isAdmin = auth.user.role === 'MANAGER' || auth.user.role === 'ADMIN';
+    const {setData, post:postComment, delete:deleteFn, errors} = useForm({
+        content: '',
+    });
+    const submit = (e) => {
+        e.preventDefault();
+        postComment(route('posts.comment.store', {id: post.id}));
+    }
+    const deleteComment = (e, comment_id) => {
+        e.preventDefault();
+        deleteFn(route('posts.comment.destroy', {id:post.id, comment_id}));
+    }
+    const deletePost = (e) => {
+        e.preventDefault();
+        deleteFn(route('posts.destroy', {id:post.id}));
+    }
     return (
         <AuthenticatedLayout user={auth.user}>
             <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
@@ -22,13 +42,62 @@ export default function ShowPost({auth, post, author}){
 
                             by {author}
                         </p>
+                        { (isAuthor || isAdmin) &&
+                            <PrimaryButton onClick={(e) => deletePost(e)}>
+                            <p className=' text-sm'>
+                                Delete
+                            </p>
+                        </PrimaryButton>
+                        }
+
                     </div>
                     <div>
                         <p className="">
                             {post.body}
                         </p>
                     </div>
+                <div className='flex flex-col justify-between my-10'>
+                    <div className='flex items-center space-x-10'>
+                        <h1 className="text-center text-md font-bold text-blue-600 sm:text-lg">
+                            Comments
+                        </h1>
+                        <p className='text-gray-600 text-sm'>
+                            {post?.comments.length} comments
+                        </p>
+                    </div>
 
+                    {post?.comments?.map((comment,i) => (
+                        <div className='flex items-center justify-between my-10' key={i}>
+                            <div className='flex flex-col justify-between space-y-2'>
+                                <div className='flex space-x-6'>
+                                    <p className='underline text-gray-700'>
+                                        <Link href={route('posts.user-index', {id: comment.user.id})}>
+                                            {comment.user.name}
+                                        </Link>
+                                    </p>
+                                    <PrimaryButton onClick={(e)=>deleteComment(e,comment.id)} >
+                                        <p className=' text-sm'>
+                                            Delete
+                                        </p>
+                                    </PrimaryButton>
+                                </div>
+                             <p className=' text-md'>
+                                {comment.content}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
+                    <form className='mt-10 w-full' onSubmit={(e)=>submit(e)}>
+                        <TextArea className='w-full'
+                          onChange={(e) => setData('content', e.target.value)}
+                        />
+                        <InputError message={errors.content} className="mt-2"/>
+                        <PrimaryButton className='mt-4' >
+                            Post Comment
+                        </PrimaryButton>
+
+                    </form>
+                </div>
                 </div>
             </div>
 
